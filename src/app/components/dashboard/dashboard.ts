a8414646ddfe9d7d926b00cbff3f29684a2973fe
Daniel <damen02.info@gmail.com>
2026-05-28 20:50:25 +0200

    feature - dinamic panel

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+  // We use a signal to manage the locked state of the UI
+  readonly isLocked = signal<boolean>(false);
+
+  // Method for the "Launch Test" button
+  onLaunchTest(): void {
+    this.isLocked.set(true);
+    console.log('UI Bloqueada de forma preventiva.');
+
+    // Simulate an asynchronous operation that will unlock the UI after 3 seconds
+    setTimeout(() => {
+      this.isLocked.set(false);
+      console.log('UI Desbloqueada automáticamente.');
+    }, 3000);
+  }
+}
