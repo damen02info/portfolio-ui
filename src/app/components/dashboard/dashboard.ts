34e3e936b14d127ffcfa5585816e7e56e891a1ca
Daniel <damen02.info@gmail.com>
2026-05-28 21:54:47 +0200

    feature - Http client connection

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Deployment } from '../../services/deployment';
+import { DeployRequest } from '../../services/deployment.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +9,48 @@ import { Component, signal } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  private readonly deploymentService = inject(Deployment);
+
   // We use a signal to manage the locked state of the UI
   readonly isLocked = signal<boolean>(false);
 
-  // Method for the "Launch Test" button
-  onLaunchTest(): void {
-    this.isLocked.set(true);
-    console.log('UI Bloqueada de forma preventiva.');
+  private currentDeploymentId: string | null = null;
 
-    // Simulate an asynchronous operation that will unlock the UI after 3 seconds
-    setTimeout(() => {
-      this.isLocked.set(false);
-      console.log('UI Desbloqueada automáticamente.');
-    }, 3000);
+  onLaunchDeployment(): void {
+    // UI block to prevent running multiple deployments at the same time
+    this.isLocked.set(true);
+
+    const payload: DeployRequest = {
+      project: 'portfolio-main-website',
+    };
+
+    console.log('Solicitando despliegue para el proyecto: ' + payload.project);
+
+    // We subscribe to the Observable returned by the deployment service to handle the asynchronous response
+    this.deploymentService.launchDeployment(payload).subscribe({
+      next: (response) => {
+        this.currentDeploymentId = response.deploymentId;
+        console.log(
+          'Pipeline aceptado por Spring Boot de forma asíncrona. Track ID: ' +
+            this.currentDeploymentId,
+        );
+      },
+      error: (errorResponse) => {
+        if (errorResponse.status === 423) {
+          console.warn(
+            'El servidor rechazó la solicitud: El sistema ya se encuentra bloqueado por otra tarea.',
+          );
+        } else if (errorResponse.status === 400) {
+          console.error('Error en la petición: Parámetro "project" ausente o incorrecto.');
+        } else {
+          console.error(
+            'Error de comunicación o fallo de red general con el backend:',
+            errorResponse,
+          );
+        }
+        // Unlock the UI in case of error
+        this.isLocked.set(false);
+      },
+    });
   }
 }
--- src/app/services/deployment.ts
+++ src/app/services/deployment.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { DeployRequest, DeployResponse } from './deployment.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Deployment {}
+export class Deployment {
+  private readonly apiUrl = 'http://localhost:8080/api/deploy';
+
+  private readonly http = inject(HttpClient);
+
+  launchDeployment(payload: DeployRequest): Observable<DeployResponse> {
+    return this.http.post<DeployResponse>(this.apiUrl, payload);
+  }
+}
